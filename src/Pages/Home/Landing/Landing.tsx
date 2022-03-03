61245b34517edecf5b6ce78caa9db61d4ee7dfaf
Abdul Hakim <abdulhakimefti@gmail.com>
2022-03-03 20:34:20 +0600

    aaa

--- src/Pages/Home/Landing/Landing.tsx
+++ src/Pages/Home/Landing/Landing.tsx
@@ -1,7 +1,7 @@
 import { Typography } from "@mui/material";
 import React from "react";
 import illustration from "../../../Media/illustrations.png";
-import './landing.css'
+import "./landing.css"
 
 const Landing = () => {
   return (
@@ -10,25 +10,34 @@ const Landing = () => {
       <div style={{ backgroundColor: "#556270", height: "300px" }}></div>
       <div style={{ display: "flex", margin: "-525px 5% 94px 5%" }}>
         <div style={{ width: "50%" }}>
-          <img style={{ width: "80%" }} src={illustration} />
+          <img className="img" src={illustration} />
         </div>
         <div style={{ width: "50%", color: "#fff" }}>
           <div>
-            <Typography style={{ fontSize: "60px", fontWeight: "700" }}>
+            <h1 className='headingOne'>
               It’s never too late to learn!
-            </Typography>
+            </h1>
           </div>
-          <div style={{ margin: "120px 0 0 0", width: "70%" }}>
-            <Typography style={{ fontSize: "20px", fontWeight: "500" }}>
+          <div className='details' style={{ margin: "120px 0 0 0", width: "100%" }}>
+            <p className="loadingPara" >
               The smart way to manage lorem ipsum dolor sit amet consectetuer
               adipiscing
-            </Typography>
-            <Typography style={{ margin: "50px 0 0 0", fontSize: "14px" }}>
+            </p>
+            <p className="loadingPara2" >
               Lorem ipsum dolor sit amet, consectetuer adipiscing elit{" "}
-            </Typography>
+            </p>
           </div>
         </div>
       </div>
+      <div className='detailsRes' style={{ margin: "120px 0 0 0", width: "100%",color:'white' }}>
+           <div style={{margin:'10%'}}> <p className="loadingPara" >
+              The smart way to manage lorem ipsum dolor sit amet consectetuer
+              adipiscing
+            </p>
+            <p className="loadingPara2" >
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit{" "}
+            </p></div>
+          </div>
     </div>
   );
 };
