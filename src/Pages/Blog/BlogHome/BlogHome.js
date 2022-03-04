903e4460f83fede4d151ed36cf09d0fabaacd66e
Abdul Hakim <abdulhakimefti@gmail.com>
2022-03-04 12:42:52 +0600

    done

--- src/Pages/Blog/BlogHome/BlogHome.js
+++ src/Pages/Blog/BlogHome/BlogHome.js
@@ -5,84 +5,86 @@ import './BlogHome.css'
 
 const BlogHome = () => {
     return (
-        <div style={{ marginLeft: '14%',marginBottom:'50px',marginTop:'25px' }}>
+        <div className='blogContainer' style={{ marginBottom: '50px', marginTop: '25px' }}>
             <Grid container>
                 <Grid item xs={12} sm={12} md={6}>
-                    <div>
+                    <div className='eventContainer' >
+                        <div >
 
-                        <Typography variant='span' style={{ fontWeight: '700', backgroundColor: '#F4DDDF', color: "red", }}>BLOG</Typography>
-                        <div style={{ margin: "20px 0" }}><Typography variant='span' style={{ fontSize: '48px', color: "#556270", }}>Latest News</Typography></div>
+                            <span className='blog' variant='span' style={{ fontWeight: '700', backgroundColor: '#F4DDDF', color: "red", }}>BLOG</span>
+                            <div className='blogHeadingDiv' ><h1 className='blogHeading' style={{ color: "#556270", }}>Latest News</h1> </div>
 
+
+                        </div>
+
+                        <Grid container>
+                            <Grid item xs={4} sm={4} md={3}>
+                                <span className='blogDate' style={{ fontWeight: '500', backgroundColor: 'red', color: "white", }}>14 feb 2022</span>
+                                <div className='borderHeight' style={{ borderLeft: '2px solid red', margin: '0 45px' }}></div>
+                            </Grid>
+                            <Grid item xs={8} sm={8} md={9}>
+                                <p className='eventHeading'>Turning customers into partners and friends
+                                </p>
+                                <p className='eventPara' >Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam adipiscing elit, sed diam consectetuer adipiscing elit, sed diam adipiscing elit, sed diam...
+                                </p>
+                            </Grid>
+                        </Grid>
+                        <Grid container>
+                            <Grid item xs={4} sm={4} md={3}>
+                                <span className='blogDate' style={{ fontWeight: '500', backgroundColor: 'red', color: "white", }}>11 jan 2022</span>
+                                <div className='borderHeight' style={{ borderLeft: '2px solid red', margin: '0 45px' }}></div>
+                            </Grid>
+                            <Grid item xs={8} sm={8} md={9}>
+                                <p className='eventHeading'>Improving usability to increase satisfaction
+                                </p>
+                                <p className='eventPara' >Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam adipiscing elit, sed diam consectetuer adipiscing elit, sed diam adipiscing elit, sed diam...
+                                </p>
+                            </Grid>
+                        </Grid>
+                        <Grid container>
+                            <Grid item xs={4} sm={4} md={3}>
+                                <span className='blogDate' style={{ fontWeight: '500', backgroundColor: 'red', color: "white", }}>11 dec 2021</span>
+                                <div className='borderHeightD' style={{ borderLeft: '2px solid red', margin: '0 45px' }}></div>
+                            </Grid>
+                            <Grid item xs={8} sm={8} md={9}>
+                                <p className='eventHeading'>Using data analysis insight to inform decisions
+
+                                </p>
+                                <p className='eventPara' >Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam adipiscing elit, sed diam consectetuer adipiscing elit, sed diam adipiscing elit, sed diam...
+                                </p>
+                            </Grid>
+                        </Grid>
 
                     </div>
-
-                    <Grid container>
-                        <Grid item md={3}>
-                            <Typography variant='span' style={{ padding: '2px 5px', fontWeight: '500', backgroundColor: 'red', color: "white", }}>14 feb 2022</Typography>
-                            <div style={{ borderLeft: '2px solid red', height: '200px', margin: '0 45px' }}></div>
-                        </Grid>
-                        <Grid item md={9}>
-                            <Typography style={{ fontSize: '24px', width: "70%" }}>Turning customers into partners and friends
-                            </Typography>
-                            <Typography style={{ fontSize: '14px', width: "70%", marginTop: '25px' }}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam adipiscing elit, sed diam consectetuer adipiscing elit, sed diam adipiscing elit, sed diam...
-                            </Typography>
-                        </Grid>
-                    </Grid>
-                    <Grid container>
-                        <Grid item md={3}>
-                            <Typography variant='span' style={{ padding: '2px 5px', fontWeight: '500', backgroundColor: 'red', color: "white", }}>11 jan 2022</Typography>
-                            <div style={{ borderLeft: '2px solid red', height: '200px', margin: '0 45px' }}></div>
-                        </Grid>
-                        <Grid item md={9}>
-                            <Typography style={{ fontSize: '24px', width: "70%" }}>Improving usability to increase satisfaction
-                            </Typography>
-                            <Typography style={{ fontSize: '14px', width: "70%", marginTop: '25px' }}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam adipiscing elit, sed diam consectetuer adipiscing elit, sed diam adipiscing elit, sed diam...
-                            </Typography>
-                        </Grid>
-                    </Grid>
-                    <Grid container>
-                        <Grid item md={3}>
-                            <Typography variant='span' style={{ padding: '2px 5px', fontWeight: '500', backgroundColor: 'red', color: "white", }}>11 dec 2021</Typography>
-                            <div style={{ borderLeft: '2px solid red', height: '250px', margin: '0 45px' }}></div>
-                        </Grid>
-                        <Grid  item md={9}>
-                            <Typography style={{ fontSize: '24px', width: "70%" }}>Using data analysis insight to inform decisions
-
-                            </Typography>
-                            <Typography style={{ fontSize: '14px', width: "70%", marginTop: '25px' }}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam adipiscing elit, sed diam consectetuer adipiscing elit, sed diam adipiscing elit, sed diam...
-                            </Typography>
-                        </Grid>
-                    </Grid>
-
 
 
                 </Grid>
-                <Grid item xs={12} sm={12} md={6}>
-                    <div style={{ backgroundColor: '#FF6B6B', height: '800px', }}>
+                <Grid className='dynamicImage' item xs={12} sm={12} md={6}>
+                    <div className='imageContainer' style={{ backgroundColor: '#FF6B6B', }}>
                         <div style={{ display: 'flex', justifyContent: 'center' }}>
-                            <img style={{ width: '85%', marginTop: "50px" }} src={blogImage} />
+                            <img className='imageBlog' src={blogImage} />
                         </div>
                     </div>
-                   <div style={{display:'flex',justifyContent:"center"}}>
+                    <div style={{ display: 'flex', justifyContent: "center" }}>
 
-                   <div style={{ width: "400px",height:"300px",backgroundColor:'white',marginTop:"-350px",zIndex:'1000' }}>
-                        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                            <Typography variant='span' style={{ backgroundColor: '#556270', color: 'white', padding: '8px 14px',marginTop:"-15px" }}>DECEMBER 2021</Typography>
+                        <div className='imageCard' style={{ backgroundColor: 'white', zIndex: '1000' }}>
+                            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                <span className='imageDate' style={{ backgroundColor: '#556270', color: 'white', marginTop: "-15px" }}>DECEMBER 2021</span>
 
-                        </div>
-                        <div style={{ display: 'flex', justifyContent: 'center' }}>
-                            <div style={{ width: "90%" }}>
-                                <Typography style={{ fontSize: '32px', textAlign: 'center', margin: '15px 0',color:'#556270' }}>Building real relationships
-                                </Typography>
-                                <div>
-                                    <Typography style={{ fontSize: '14px', textAlign: 'center' }}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
-                                    </Typography>
-                                    
+                            </div>
+                            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                                <div style={{ width: "90%" }}>
+                                    <p className='cardHeading' style={{ textAlign: 'center', color: '#556270' }}>Building real relationships
+                                    </p>
+                                    <div>
+                                        <p className='cardPara' style={{ textAlign: 'center' }}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+                                        </p>
+
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                   </div>
                 </Grid>
             </Grid>
         </div>
